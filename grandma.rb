136615b783b6a9406f83c6d72bb4b909a6144752
Kameron Hungerford <kam.hungerford@gmail.com>
2020-07-08 22:57:04 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,8 +13,6 @@
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
 
-require "spec_helper"
-
 def speak_to_grandma(phrase)
    if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
