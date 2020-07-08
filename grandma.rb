669710955b85070ec570ac652263380140a88f97
Kameron Hungerford <kam.hungerford@gmail.com>
2020-07-08 22:51:07 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,7 +3,7 @@ def speak_to_grandma(phrase)
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
-
+  if phrase == "I LOVE YOU GRANDMA!"
 
 # If you shout, she can hear you (or at least she thinks so) 
 # and yells back
