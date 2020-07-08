9ef1a37bfd25f9712c7f9c373aaee514e5c677c8
Kameron Hungerford <kam.hungerford@gmail.com>
2020-07-08 23:00:24 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -20,6 +20,8 @@ def speak_to_grandma(phrase)
     return "HUH?! SPEAK UP SONNY!"
   elsif phrase == phrase.downcase
     return "HUH?! SPEAK UP SONNY!"
+  elsif phrase == phrase.downcase
+    return "HUH?! SPEAK UP SONNY!"
   else phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
   end
