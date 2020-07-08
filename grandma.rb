5745ac05ef3d769c9eaaacf50ebe93e6926a56bd
Kameron Hungerford <kam.hungerford@gmail.com>
2020-07-08 22:51:38 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,7 +14,7 @@ def speak_to_grandma(phrase)
 # 'I LOVE YOU TOO PUMPKIN!'
 
 
-  
+   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   else
     puts "HUH?! SPEAK UP, SONNY!"
